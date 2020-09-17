42e8ccd9f7b0ee670b728c59be325662375c4fb0
LiamGallagher03 <71441726+LiamGallagher03@users.noreply.github.com>
2020-09-17 08:23:53 -0400

    Fully completed

--- QuizApp.java
+++ QuizApp.java
@@ -21,22 +21,42 @@ public class QuizApp {
   int answer2 = kboard.nextInt();
   if (answer2 == 3) {
       score++;
-      System.out.println("Corrct!");
+      System.out.println("Correct!");
   }
   else {
       System.out.println("I hope you typed the word instead of actually getting it wrong");
   }
   System.out.println("What is 9+10?");
   int answer3 = kboard.nextInt();
-          if (answer3 == 19);
-          {
+          if (answer3 == 19) {
            System.out.println("Correct!");
+           score++;
           }
           else {
            System.out.println("Math is hard.");   
                    }
-           
-          
+     System.out.println("What is the color of grass?");
+        String answer4 = kboard.next();
+        
+         if (answer4.equals("green"))
+         {
+             score++;
+             System.out.println("Corect!");
+         }
+         else {
+             System.out.println("Incorrect!");
+         }
+    System.out.println("How many days are in a week?");
+    int answer5 = kboard.nextInt();
+    if (answer5 == 7) {
+        System.out.println("Correct!");
+                score++;
+                System.out.println("Your final score is "+score+".");
+    }
+    else {
+        System.out.println("Incorrect!");
+        System.out.println("Your final score is "+score+".");
+    }
     }
     
 }
